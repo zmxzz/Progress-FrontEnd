97329af8b9620b636146acf265130799e680b6d7
zmxzz <clintlyj@gmail.com>
2019-05-29 21:47:20 -0500

    Half Login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { 
+  MatToolbarModule,
+  MatSidenavModule, 
+  MatButtonModule, 
+  MatIconModule, 
+  MatListModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
